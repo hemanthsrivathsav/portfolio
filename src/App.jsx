02a5d0a5b379a4e34235c8cb1518d9a954157be3
Hemanth Srivathsav <hemanthsrivathsav@gmail.com>
2024-12-27 19:43:47 +0530

    added country video

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { FiSmile } from 'react-icons/fi';
 
 import './App.css'
 
+import Powerslap from "./assets/Country2.mp4"
 
 
 
@@ -239,8 +240,12 @@ function App() {
             
           </div>
         
-        <div className=' bg-white rounded-md '>
-          <h1>This is below </h1>
+        <div className=' bg-slate-800  rounded-md '>
+          <div className=' p-2'>
+            <video className='rounded-lg'autoPlay loop muted src="src\assets\Country.mp4"  width="320" height="240" >
+              <source src={Powerslap} type="video/mp4" />
+            </video>
+          </div>
         </div>
       </div>
       
